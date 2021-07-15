e0399ff1983ab1e2ffba0b707e58c86433957706
Pablo Coronel <me@pablitxn.io>
2021-07-14 21:54:29 -0300

    feat(navigation): create tab component

--- navigation/status-tab/index.tsx
+++ navigation/status-tab/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { TabAll, TabCompleted, TabFavorite, TabUncompleted } from './tabs'
 
-const BottomTab = createBottomTabNavigator<any>()
+const TopTab = createMaterialTopTabNavigator<any>()
 
 const BottomTabNavigator = () => {
   return (
-    <BottomTab.Navigator initialRouteName="all">
-      <BottomTab.Screen name="all" component={TabAll} />
-      <BottomTab.Screen name="completed" component={TabCompleted} />
-      <BottomTab.Screen name="favorite" component={TabFavorite} />
-      <BottomTab.Screen name="uncompleted" component={TabUncompleted} />
-    </BottomTab.Navigator>
+    <TopTab.Navigator initialRouteName="all">
+      <TopTab.Screen name="all" component={TabAll} />
+      <TopTab.Screen name="completed" component={TabCompleted} />
+      <TopTab.Screen name="favorite" component={TabFavorite} />
+      <TopTab.Screen name="uncompleted" component={TabUncompleted} />
+    </TopTab.Navigator>
   )
 }
 
